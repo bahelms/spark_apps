c4a5aef98e8b001a0c67c2e958f05c44cf2766d9
bahelms <jimbonkgit@gmail.com>
2016-09-25 10:29:03 -0400

    finishes state count for specific CSV file

--- state_count/src/main/scala/StateCount.scala
+++ state_count/src/main/scala/StateCount.scala
@@ -2,6 +2,18 @@ import spark_base.SparkBase
 
 object StateCount extends SparkBase {
   def main(args: Array[String]) = {
-    println(s"ARGS: ${args(0)}")
+    val regex: String = """,(?=([^\"]*\"[^\"]*\")*[^\"]*$)"""
+    val file = sc.textFile(s"${args(0)}/${args(1)}")
+
+    val header = file.first
+    val results = file
+      .filter(_ != header)
+      .map(_ split regex)
+      .map(row => row.map(_.replaceAll("""\s+""", "")))
+      .map(_.zipWithIndex)
+      .map(row => (row(7)._1.toUpperCase, 1))
+      .reduceByKey(_ + _)
+
+    results.saveAsTextFile(s"${args(0)}/scala/spark_apps/state_count/output")
   }
 }
